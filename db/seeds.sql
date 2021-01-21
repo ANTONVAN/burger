9d39bd428199ae554361ef3f25e1526ffd42d362
Antonio <aantonio_94@hotmail.com>
2021-01-20 17:41:44 -0700

    first commiT

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Mexican burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Extreme burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double cheese and bacon burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Egg burger');
